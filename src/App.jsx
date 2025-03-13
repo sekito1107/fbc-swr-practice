4c3d9052e605343f50d2857ddee6e7c38d3d1436
sekito <sekito1107gg@gmail.com>
2025-03-13 23:22:29 +0900

    取得したdetaのdescriptionを表示するように変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import useSWR from "swr";
 import "./App.css";
 
-const fetcher = (...args) => fetch(...args);
+const fetcher = ([url, options]) =>
+  fetch(url, options).then((res) => res.json());
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
+  const options = { headers: { Accept: "application/json" } };
 
-  const { data, error, isLoading } = useSWR([url, headers], fetcher);
+  const { data, error, isLoading } = useSWR([url, options], fetcher);
 
   return (
     <>
       {isLoading && <p>Loading...</p>}
-      {data && <p>Status: OK</p>}
+      {data && <p>Status: {data.description}</p>}
       {error && <p>Failed to load.</p>}
     </>
   );
